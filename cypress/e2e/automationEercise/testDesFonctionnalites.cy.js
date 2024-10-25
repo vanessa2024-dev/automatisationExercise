29ca207235c244c49a3fd691e312e00631fdfecf
vanessa2024-dev <tientcheuvanessa27@gmail.com>
2024-10-25 15:04:05 +0100

    ajout d un cas de test et modification du cas de test : tester la page des cas de tests

--- cypress/e2e/automationEercise/testDesFonctionnalites.cy.js
+++ cypress/e2e/automationEercise/testDesFonctionnalites.cy.js
@@ -35,25 +35,69 @@ describe('tester les differentes fonctionnalites du site', ()=>{
 
         cy.contains('If you have any suggestion areas or improvements, do let us know. We will definitely work on it.')
         cy.contains('Thank you')
-       cy.get('[class="btn btn-primary pull-left submit_form"]').contains('Submit').should('be.visible').click()
+        cy.get('[class="btn btn-primary pull-left submit_form"]').contains('Submit').should('be.visible').click()
         cy.contains('Success! Your details have been submitted successfully.')
         cy.get('#form-section > .btn').contains('Home').should('be.visible').click()
 
 
 
     })
-    it('verifer la page des cas de tests', ()=>{
-        cy.get('.active > :nth-child(1) > .test_cases_list > .btn').click()
+    it('verifer la page des cas de tests le body sur', ()=>{
+        cy.contains('AutomationExercise')
+        cy.contains('Full-Fledged practice website for Automation Engineers')
+        cy.contains('All QA engineers can use this website for automation practice and ')
+        cy.contains(' either they are at beginner or advance level. This is for everybody to help them brush up their automation skills.')
+        cy.contains('APIs list for practice')
+        cy.get('[ class="test_cases_list"]').contains('Test Cases').should('be.visible').click()
         cy.contains('Test Cases')
         cy.contains('Below is the list of test Cases for you to practice the Automation. Click on the scenario for detailed Test Steps:')
+        cy.get('[ class="panel panel-default"]').contains('Feedback for Us').should('be.visible')
+        cy.get('[id="feedback"]').should('be.visible')
+        cy.contains('We have identified above scenarios and added in the list.')
+        cy.contains('You can explore more test cases in the website and if you find new test scenario that is not covered in above list, do let us know. We will definitely add that in above list.')
+        // cy.contains('If you think, this website should cover up any particular feature, kindly share with us at feedback@automationexercise.com . We will work on that part. Your feedback matters a lot. ')
+        // cy.get('[class="container"]').find('div').should('equal',27  )
+        cy.contains('Test Case 1: Register User')
+        cy.contains('Test Case 2: Login User with correct email and password')
+        cy.contains('Test Case 3: Login User with incorrect email and password')
+        cy.contains('Test Case 4: Logout User')
+        cy.contains('Test Case 5: Register User with existing email')
+        cy.contains('Test Case 6: Contact Us Form')
+        cy.contains('Test Case 7: Verify Test Cases Page')
+        cy.contains('Test Case 8: Verify All Products and product detail page')
+        cy.contains('Test Case 9: Search Product')
+        cy.contains('Test Case 10: Verify Subscription in home page')
+        cy.contains('Test Case 11: Verify Subscription in Cart page')
+        cy.contains('Test Case 12: Add Products in Cart')
+        cy.contains('Test Case 13: Verify Product quantity in Cart')
+        cy.contains('Test Case 14: Place Order: Register while Checkout')
+        cy.contains('Test Case 15: Place Order: Register before Checkout')
+        cy.contains('Test Case 16: Place Order: Login before Checkout')
+        cy.contains('Test Case 17: Remove Products From Cart')
+        cy.contains('Test Case 18: View Category Products')
+        cy.contains('Test Case 19: View & Cart Brand Products')
+        cy.contains('Test Case 20: Search Products and Verify Cart After Login')
+        cy.contains('Test Case 21: Add review on product')
+        cy.contains('Test Case 22: Add to cart from Recommended items')
+        cy.contains('Test Case 23: Verify address details in checkout page')
+        cy.contains('Test Case 24: Download Invoice after purchase order')
+        cy.contains("Test Case 25: Verify Scroll Up using 'Arrow' button and Scroll Down functionality")
+        cy.contains("Test Case 26: Verify Scroll Up without 'Arrow' button and Scroll Down functionality")
+        
 
 
 
 
     })
+    it('verifier la page des cas de tests sur le header',()=>{
+        cy.get('.active > :nth-child(1) > .test_cases_list > .btn').click()
+        cy.contains('Test Cases')
+        cy.contains('Below is the list of test Cases for you to practice the Automation. Click on the scenario for detailed Test Steps:')
+
+    })
 
 
-    it.only('verifer la page des produits', ()=>{
+    it('verifer la page des produits', ()=>{
         // buton product
         cy.get('.shop-menu > .nav > :nth-child(2)').click()
         // buton de recherche
@@ -128,6 +172,19 @@ describe('tester les differentes fonctionnalites du site', ()=>{
         
        
 
+        
+    })
+    it.only(' Verifier la souscription sur la page d accueil', ()=>{
+
+        cy.get('[id="footer"]').scrollIntoView().should('be.visible')
+        cy.contains('Subscription')
+        cy.contains('Get the most recent updates from our site and be updated your self...')
+        cy.get('[id="susbscribe_email"]').should('be.visible').type('vanessa@gmail.com')
+        cy.get('[id="subscribe"]').should('be.visible').click()
+        cy.contains('You have been successfully subscribed!')
+    })
+
+    it('', ()=>{
         
     })
 })
--- cypress/e2e/automationEercise/testNegatif.cy.js
+++ cypress/e2e/automationEercise/testNegatif.cy.js
@@ -40,4 +40,6 @@ describe('test negatif' , ()=>{
 
 
     })
+    
+    
 })
